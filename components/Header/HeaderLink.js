b4d6fe5a54f96b3ba4e561d838acae92f74cfe9a
TheGraveyardWarden <ali.manutd10r@gmail.com>
2023-05-24 18:01:45 +0330

    add hover effect to header_link

--- components/Header/HeaderLink.js
+++ components/Header/HeaderLink.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 function HeaderLink({name, to, className, onClick, children}) {
     return <div name={name} onClick={onClick} className={`header_link ${className}`} >
+        <div className="header_link_bg"></div>
         <Link to={to}>{children}</Link>
     </div>
 }
